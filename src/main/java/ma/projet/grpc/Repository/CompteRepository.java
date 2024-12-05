6c45a830b28e96b653e9eb9e0db6fdf98c5d2f60
mohcine15 <mohcinesalhi7@gmail.com>
2024-12-05 23:17:54 +0100

    premier

---
+++ src/main/java/ma/projet/grpc/Repository/CompteRepository.java
@@ -0,0 +1,11 @@
+package ma.projet.grpc.Repository;
+
+import ma.projet.grpc.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CompteRepository extends JpaRepository<Compte, String> {
+    List<Compte> findAllByType(String type);
+
+}
